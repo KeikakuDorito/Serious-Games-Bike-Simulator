99097beb27cb315e94fa0ed47cca4f6c216a86b9
Abdalla Mohamed <abdulla.mohamed.twitter@gmail.com>
2023-11-29 00:33:09 -0500

    Helmet and Pump tasks

--- SeriousGamesBikeSimulator/Assets/HelmtedtoHead.cs
+++ SeriousGamesBikeSimulator/Assets/HelmtedtoHead.cs
@@ -7,22 +7,44 @@ public class HelmtedtoHead : MonoBehaviour
     // Start is called before the first frame update
 
     public Transform head;
+    public GameObject selectedObject;
+    Camera Cam;
     Vector3 myVector = new Vector3 (0f,0.27f,-0.81f);
+    public LayerMask mask;
 
-    void Start()
+
+
+    private void OnTriggerEnter(Collider other)
     {
-       
+       if(other.CompareTag("Player"))
+        {
+            selectedObject = gameObject;
+        }
+        
+      
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            selectedObject = null;
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && selectedObject != null)
+        {
+        
+
+                gameObject.transform.position = head.position + (head.up * 0.4f) + (head.forward * 0.6f);
+                gameObject.transform.parent = head;
+
+        }
     }
 
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.E))
-        {
-            gameObject.transform.position = head.position + (head.up * 0.4f) + (head.forward * 0.6f);
-            gameObject.transform.parent = head;
-
-        }
-    }
+    
 }
